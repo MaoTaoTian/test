cccbc57921fbc0aa96e2380af168f8caee2c67d2
maotaotian <maotaotian@yeah.net>
2019-06-10 09:44:35 +0800

    Test

--- TestDemo/TestDemo/ViewController.m
+++ TestDemo/TestDemo/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.collectionView];
+    
 }
 
 - (void)viewDidLayoutSubviews {
