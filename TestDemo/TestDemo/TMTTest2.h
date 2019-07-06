42183fabca54a3dcff93fb884ccd1297a4e378ed
tianmaotao <maotaotian@yeah.net>
2019-07-06 21:46:31 +0800

    add TMTTest2

---
+++ TestDemo/TestDemo/TMTTest2.h
@@ -0,0 +1,17 @@
+//
+//  TMTTest2.h
+//  TestDemo
+//
+//  Created by tianmaotao on 2019/7/6.
+//  Copyright © 2019 tianmaotao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TMTTest2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestDemo/TestDemo/TMTTest2.m
@@ -0,0 +1,13 @@
+//
+//  TMTTest2.m
+//  TestDemo
+//
+//  Created by tianmaotao on 2019/7/6.
+//  Copyright © 2019 tianmaotao. All rights reserved.
+//
+
+#import "TMTTest2.h"
+
+@implementation TMTTest2
+
+@end
