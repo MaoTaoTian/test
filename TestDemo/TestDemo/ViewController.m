285f4ddba87079cead2885e1260c36e1141751cb
tianmaotao <tianmaotao@bilibili.com>
2020-03-02 17:31:13 +0800

    test1

--- TestDemo/TestDemo/ViewController.m
+++ TestDemo/TestDemo/ViewController.m
@@ -35,6 +35,10 @@
 
 @implementation ViewController
 
+- (void)test1 {
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
